4f8e003636f69f5ed735cc7f0bc1b943cf69bf9b
lukehayes <lukepurplehayes@gmail.com>
2024-10-09 10:24:21 +0100

    Updated main.c with fread test

--- main.c
+++ main.c
@@ -3,7 +3,23 @@
 int main() 
 {
     
-    printf("Loaded \n");
+    int bufSize = 300;
+    char buffer[bufSize];
+
+    FILE* fh = fopen("../assets/map.lua", "r");
+
+    fread(buffer, 1, bufSize, fh);
+    char* c = buffer;
+
+    while(*c != '=')
+    {
+        printf("%c \n", *c);
+        c++;
+    }
+
+    fclose(fh);
+
+    // printf("Buffer: %s \n", buffer);
 
     return 0;
 }
